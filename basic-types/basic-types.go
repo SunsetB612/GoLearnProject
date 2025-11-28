1a8dbd4295ad2d046f00ba2a75a4d90eadca1466
SunsetB612 <10235101575@stu.ecnu.edu.cn>
2025-11-28 10:56:43 +0800

    Basic Types
    Signed-off-by: SunsetB612 <10235101575@stu.ecnu.edu.cn>

---
+++ basic-types/basic-types.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx" //Go 的 复数数学库，提供各种针对复数的数学函数
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	fmt.Printf("类型：%T 值：%v\n", ToBe, ToBe)
+	fmt.Printf("类型：%T 值：%v\n", MaxInt, MaxInt)
+	fmt.Printf("类型：%T 值：%v\n", z, z)
+}
